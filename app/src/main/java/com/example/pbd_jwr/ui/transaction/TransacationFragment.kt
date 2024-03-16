bdc3e65f2a053738d62898d0e3599105e14cda87
zaydanA <13521104@std.stei.itb.ac.id>
2024-03-16 21:30:47 +0700

    intialize

---
+++ app/src/main/java/com/example/pbd_jwr/ui/transaction/TransacationFragment.kt
@@ -0,0 +1,42 @@
+package com.example.pbd_jwr.ui.transaction
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.pbd_jwr.databinding.FragmentTransactionBinding
+
+class TransacationFragment : Fragment() {
+
+    private var _binding: FragmentTransactionBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this)[TransactionViewModel::class.java]
+
+        _binding = FragmentTransactionBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textTransaction
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/pbd_jwr/ui/transaction/TransactionViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.pbd_jwr.ui.transaction
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TransactionViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is transaction Fragment"
+    }
+    val text: LiveData<String> = _text
+}
